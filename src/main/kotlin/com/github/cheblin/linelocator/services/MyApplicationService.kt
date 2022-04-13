2b9d0db04045e2d2aa9209fd586f34709db0a054
GitHub Action <action@github.com>
2022-04-13 09:15:20 +0000

    Template cleanup

--- src/main/kotlin/com/github/cheblin/linelocator/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/cheblin/linelocator/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.cheblin.linelocator.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.cheblin.linelocator.MyBundle
 
 class MyApplicationService {
 
--- src/main/kotlin/com/github/cheblin/linelocator/services/MyProjectService.kt
+++ src/main/kotlin/com/github/cheblin/linelocator/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.cheblin.linelocator.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.cheblin.linelocator.MyBundle
 
 class MyProjectService(project: Project) {
 
